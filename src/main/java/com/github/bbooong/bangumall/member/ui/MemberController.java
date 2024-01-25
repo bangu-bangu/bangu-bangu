3fdea5932e481a59bb8ab11b38a844803906ad7e
sen <seeunchoi99@gmail.com>
2024-01-25 16:09:09 +0900

    test: CreateMember 성공 테스트 통과

--- src/main/java/com/github/bbooong/bangumall/member/ui/MemberController.java
+++ src/main/java/com/github/bbooong/bangumall/member/ui/MemberController.java
@@ -1,9 +1,12 @@
 package com.github.bbooong.bangumall.member.ui;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/members")
@@ -11,5 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     @PostMapping
-    public void createMember() {}
+    public ResponseEntity<Void> createMember() {
+        final Long memberId = 777L;
+
+        return ResponseEntity.created(URI.create("/members/" + memberId)).build();
+    }
 }
