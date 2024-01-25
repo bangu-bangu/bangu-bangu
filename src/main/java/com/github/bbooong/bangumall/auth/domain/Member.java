349a9ee0c819fcf203df0be4d609a6385928a8d8
sen <seeunchoi99@gmail.com>
2024-01-25 17:18:08 +0900

    feat: Member, MemberRepository 생성

---
+++ src/main/java/com/github/bbooong/bangumall/auth/domain/Member.java
@@ -0,0 +1,24 @@
+package com.github.bbooong.bangumall.auth.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
