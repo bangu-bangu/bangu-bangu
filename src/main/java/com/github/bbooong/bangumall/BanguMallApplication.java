67d1aab30c233ae58d3abe197401294b2d0bb87d
whatasame <97666463+whatasame@users.noreply.github.com>
2024-01-25 14:52:40 +0900

    feat: Setup project

---
+++ src/main/java/com/github/bbooong/bangumall/BanguMallApplication.java
@@ -0,0 +1,13 @@
+package com.github.bbooong.bangumall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanguMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BanguMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/bbooong/bangumall/BanguMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bbooong.bangumall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanguMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
