b07d00d840eb1ba85376d0be0987f157e8f6887c
sen <seeunchoi99@gmail.com>
2024-01-30 14:37:41 +0900

    feat: product 생성 테스트 성공

--- src/main/java/com/github/bbooong/bangumall/product/ui/ProductController.java
+++ src/main/java/com/github/bbooong/bangumall/product/ui/ProductController.java
@@ -1,9 +1,12 @@
 package com.github.bbooong.bangumall.product.ui;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/products")
@@ -11,5 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     @PostMapping
-    public void createProduct() {}
+    public ResponseEntity<Void> createProduct() {
+        return ResponseEntity.created(URI.create("/products/" + 0)).build();
+    }
 }
