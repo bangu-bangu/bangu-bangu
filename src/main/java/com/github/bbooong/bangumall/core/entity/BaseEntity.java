b6785fc998fd7a3ec0cc699bf893c6a45a91a4d5
whatasame <97666463+whatasame@users.noreply.github.com>
2024-02-01 18:08:00 +0900

    feat: `BaseEntity` 추가

---
+++ src/main/java/com/github/bbooong/bangumall/core/entity/BaseEntity.java
@@ -0,0 +1,22 @@
+package com.github.bbooong.bangumall.core.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public abstract class BaseEntity {
+
+    @CreatedDate private LocalDateTime createdAt;
+
+    @LastModifiedDate private LocalDateTime modifiedAt;
+}
