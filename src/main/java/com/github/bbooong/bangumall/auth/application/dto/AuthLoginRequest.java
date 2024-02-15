87e8b0ed425ae523e2abc1122cf5390dfca61e50
whatasame <97666463+whatasame@users.noreply.github.com>
2024-02-15 17:27:16 +0900

    feat: DTO에 validation annotation 추가

--- src/main/java/com/github/bbooong/bangumall/auth/application/dto/AuthLoginRequest.java
+++ src/main/java/com/github/bbooong/bangumall/auth/application/dto/AuthLoginRequest.java
@@ -1,3 +1,10 @@
 package com.github.bbooong.bangumall.auth.application.dto;
 
-public record AuthLoginRequest(String email, String password) {}
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record AuthLoginRequest(
+        @NotNull @NotBlank @Email String email,
+        @NotNull @NotBlank @Size(min = 1, max = 255) String password) {}
