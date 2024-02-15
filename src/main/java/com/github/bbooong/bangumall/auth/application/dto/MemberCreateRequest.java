c809d8fd40692e91e18051e7b3c5ef2c2009291a
whatasame <97666463+whatasame@users.noreply.github.com>
2024-02-15 18:28:02 +0900

    fix: 불필요한 `@NotNull` 제거

--- src/main/java/com/github/bbooong/bangumall/auth/application/dto/MemberCreateRequest.java
+++ src/main/java/com/github/bbooong/bangumall/auth/application/dto/MemberCreateRequest.java
@@ -2,9 +2,7 @@ package com.github.bbooong.bangumall.auth.application.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record MemberCreateRequest(
-        @NotNull @NotBlank @Email String email,
-        @NotNull @NotBlank @Size(min = 1, max = 255) String password) {}
+        @NotBlank @Email String email, @NotBlank @Size(min = 1, max = 255) String password) {}
--- src/main/java/com/github/bbooong/bangumall/order/application/dto/OrderLineRequest.java
+++ src/main/java/com/github/bbooong/bangumall/order/application/dto/OrderLineRequest.java
@@ -2,7 +2,5 @@ package com.github.bbooong.bangumall.order.application.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
-public record OrderLineRequest(
-        @NotNull @NotBlank @Min(1) long productId, @NotNull @NotBlank @Min(1) int quantity) {}
+public record OrderLineRequest(@NotBlank @Min(1) long productId, @NotBlank @Min(1) int quantity) {}
