6be741d21b7725f7f9d30315e0190852f30f0e2e
sen <seeunchoi99@gmail.com>
2024-02-20 18:11:28 +0900

    refactor: Stock.quantity 를 VO 객체로 변경

--- src/main/java/com/github/bbooong/bangumall/stock/domain/Stock.java
+++ src/main/java/com/github/bbooong/bangumall/stock/domain/Stock.java
@@ -1,7 +1,12 @@
 package com.github.bbooong.bangumall.stock.domain;
 
 import com.github.bbooong.bangumall.stock.exception.StockQuantityNotEnoughException;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.time.LocalDate;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -23,18 +28,18 @@ public class Stock {
     private Long productId;
 
     @Column(nullable = false)
-    private int quantity;
+    private Quantity quantity;
 
     @Column(nullable = false)
     private LocalDate expiredDate;
 
     public static Stock create(
             final long productId, final int quantity, final LocalDate expiredDate) {
-        return new Stock(null, productId, quantity, expiredDate);
+        return new Stock(null, productId, Quantity.create(quantity), expiredDate);
     }
 
     public void update(final int quantity, final LocalDate expiredDate) {
-        this.quantity = quantity;
+        this.quantity = Quantity.create(quantity);
         this.expiredDate = expiredDate;
     }
 
