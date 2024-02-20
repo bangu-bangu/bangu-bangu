aafc5199cbcb92747fe13c70a2fdb75f88132b2a
sen <seeunchoi99@gmail.com>
2024-02-20 17:36:22 +0900

    test: MariaTableLockProviderTest 비활성화

--- src/test/java/com/github/bbooong/bangumall/core/MariaTableLockProviderTest.java
+++ src/test/java/com/github/bbooong/bangumall/core/MariaTableLockProviderTest.java
@@ -4,14 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 import com.github.bbooong.bangumall.testcontainers.MariaDbTest;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+@Disabled
 @MariaDbTest
 @JdbcTest
 @DisplayName("단위 테스트: MariaTableLockProvider")
