d815adc110ecd7551270b9e12ef4ea84b2c3b258
sen <seeunchoi99@gmail.com>
2024-02-20 17:54:13 +0900

    refactor: MariaTableLockProviderTest 와일드카드 import 제거

--- src/test/java/com/github/bbooong/bangumall/core/MariaTableLockProviderTest.java
+++ src/test/java/com/github/bbooong/bangumall/core/MariaTableLockProviderTest.java
@@ -4,7 +4,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 import com.github.bbooong.bangumall.testcontainers.MariaDbTest;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
