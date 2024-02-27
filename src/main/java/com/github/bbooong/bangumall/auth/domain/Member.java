dc4ac09d037aeb63c4ab6a2baab48c5671d910db
whatasame <97666463+whatasame@users.noreply.github.com>
2024-02-27 20:54:09 +0900

    feat: Member 엔티티에 역할 추가

--- src/main/java/com/github/bbooong/bangumall/auth/domain/Member.java
+++ src/main/java/com/github/bbooong/bangumall/auth/domain/Member.java
@@ -1,6 +1,13 @@
 package com.github.bbooong.bangumall.auth.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -23,7 +30,10 @@ public class Member {
     @Column(nullable = false)
     private String password;
 
-    public static Member create(final String email, final String password) {
-        return new Member(null, email, password);
+    @Enumerated(EnumType.STRING)
+    private MemberRole role;
+
+    public static Member create(final String email, final String password, final MemberRole role) {
+        return new Member(null, email, password, role);
     }
 }
